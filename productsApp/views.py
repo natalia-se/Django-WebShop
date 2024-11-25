6ab7cd29d73587cb297cf8ede0e4d273bfd08b0f
Rao-Sabahat-Ali <raosabahat01@live.com>
2024-11-25 18:16:41 +0500

    feat: (LX1-31) add products listing

--- productsApp/views.py
+++ productsApp/views.py
@@ -1,3 +1,6 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def welcome(request):
+    
+  return render(request, 'productsApp/components/products_list.html')
--- webshop/urls.py
+++ webshop/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from productsApp import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+     path('', views.welcome, name='welcome'),
 ]
