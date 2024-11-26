5e4af32ff9c4dd769703d99430526469f889c52d
PRASHANTH NAIR P <poojapradeep20492@gmail.com>
2024-11-26 10:03:52 +0100

    commiting changes

--- productsApp/views.py
+++ productsApp/views.py
@@ -2,3 +2,7 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'productsApp/home.html')
+
+# Create your views here.
+def index(request):
+    return render(request,'productsApp/components/homepage.html')
