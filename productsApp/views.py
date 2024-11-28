81d6e27e7113bc4054640991b7d37f5c29b87e49
Pooja <poojapradeep20492@gmail.com>
2024-11-28 09:37:50 +0100

    commit
    commiting product_descr changes

--- productsApp/views.py
+++ productsApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import get_object_or_404, render
 
+from .models import Product
+
 def home(request):
     return render(request, 'productsApp/home.html')
 
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'productsApp/components/product_descr.html', {'product': product})
+# Create your views here.
