6f34bf78b7ba14e3c3a8c9b1f35cb5a08364a43f
Pooja <poojapradeep20492@gmail.com>
2024-11-28 09:37:50 +0100

    commit
    commiting product_descr changes
    
    # Conflicts:
    #	productsApp/models.py
    #	productsApp/urls.py
    #	productsApp/views.py

--- productsApp/views.py
+++ productsApp/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 from .models import Cake
 
 
+from .models import Product
+
 def home(request):
     
     cakes = Cake.objects.all()[:6]
     
     return render(request, 'productsApp/home.html', {'cakes': cakes})
   # return render(request, 'productsApp/components/products_list.html', {'cakes': cakes})
+    return render(request, 'productsApp/home.html')
+
+def product_detail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'productsApp/components/product_descr.html', {'product': product})
+# Create your views here.
