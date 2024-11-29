8b0287588608eee38ceac0978550f0f7617f9006
Nataliia Ishchenko <nataliia.ishchenko@desifer.se>
2024-11-29 10:12:53 +0100

    Added sign up functionality

---
+++ productsApp/forms.py
@@ -0,0 +1,54 @@
+from django import forms
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
+
+class SignupForm(forms.ModelForm):
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'})
+    )
+    first_name = forms.CharField(
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'})
+    )
+    last_name = forms.CharField(
+        max_length=30,
+        required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'})
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'})
+    )
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm Password'})
+    )
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Username'}),
+        }
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+        try:
+            validate_password(password)
+        except ValidationError as e:
+            self.add_error('password', e)
+        return password
+
+    def clean_confirm_password(self):
+        password = self.cleaned_data.get("password")
+        confirm_password = self.cleaned_data.get("confirm_password")
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+        return confirm_password
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already registered.")
+        return email
--- productsApp/views.py
+++ productsApp/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-from .models import Cake
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import login
+from .forms import SignupForm
 
 
 def home(request):
@@ -8,4 +9,21 @@ def home(request):
     cakes = Cake.objects.all()[:6]
     
     return render(request, 'productsApp/home.html', {'cakes': cakes})
-  # return render(request, 'productsApp/components/products_list.html', {'cakes': cakes})
+ 
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            user = User.objects.create_user(
+                username=form.cleaned_data['username'],
+                first_name=form.cleaned_data['first_name'],
+                last_name=form.cleaned_data['last_name'],
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+            )
+            login(request, user)  # Automatically log the user in after signup
+            return redirect('home')  # Redirect to the home page or another page
+    else:
+        form = SignupForm()
+    return render(request, 'productsApp/signup.html', {'form': form})
+    return render(request, 'productsApp/signup.html', {'form': form})
