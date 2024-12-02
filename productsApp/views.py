c2283e7dc93f32ef049ea66bf8e53de7d78c2f4b
Pooja <poojapradeep20492@gmail.com>
2024-12-02 10:26:24 +0100

    adding changes to views.py

--- productsApp/views.py
+++ productsApp/views.py
@@ -8,9 +8,6 @@ from .forms import SignupForm, ProfileUpdateForm
 from .models import Cake
 
 
-
-from .models import Product
-
 def home(request):
     
     cakes = Cake.objects.all()[:6]
@@ -56,6 +53,6 @@ def profile(request):
     return render(request, 'productsApp/home.html')
 
 def product_detail(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
+    product = get_object_or_404(Cake, id=product_id)
     return render(request, 'productsApp/components/product_descr.html', {'product': product})
 # Create your views here.
